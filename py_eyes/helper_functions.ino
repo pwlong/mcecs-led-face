781310083e233b4ab5f221dceef2423f8c48edd8
pwlong <github@thelongs.ws>
2014-11-07 12:49:40 -0800

    moved some functiond to another file

---
+++ py_eyes/helper_functions.ino
@@ -0,0 +1,88 @@
+int readline(int readch, char *buffer, int len){
+  static int pos = 0;
+  int rpos;
+
+  if (readch > 0) {
+    switch (readch) {
+      case '\n': // Ignore new-lines
+        break;
+      case '\r': // Return on CR
+        rpos = pos;
+        pos = 0;  // Reset position index ready for next time
+        return rpos;
+      default:
+        if (pos < len-1) {
+          buffer[pos++] = readch;
+          buffer[pos] = 0;
+        }
+    }
+  }
+  // No end of line has been found, so return -1.
+  return -1;
+}
+
+
+int post(Adafruit_8x8matrix matrix) {
+  // Right now this test always passes and it is up to the user to observe the output
+  // to determine if overything went OK
+  digitalWrite(13,HIGH);
+  matrix.clear();
+  matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+  delay(500);
+
+  digitalWrite(13,HIGH);
+  matrix.clear();
+  matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+  delay(500);
+
+  digitalWrite(13,LOW);
+  matrix.clear();
+  matrix.drawBitmap(0, 0, frown_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+  delay(500);
+
+  digitalWrite(13,HIGH);
+  matrix.clear();      // clear display
+  matrix.drawPixel(0, 0, LED_ON);  
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  digitalWrite(13,LOW);
+  matrix.clear();
+  matrix.drawLine(0,0, 7,7, LED_ON);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  digitalWrite(13,HIGH);
+  matrix.clear();
+  matrix.drawRect(0,0, 8,8, LED_ON);
+  matrix.fillRect(2,2, 4,4, LED_ON);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  digitalWrite(13,LOW);
+  matrix.clear();
+  matrix.drawCircle(3,3, 3, LED_ON);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  digitalWrite(13,HIGH);
+  matrix.setTextSize(1);
+  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextColor(LED_ON);
+  for (int8_t x=0; x>=-36; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("Hello");
+    matrix.writeDisplay();
+    delay(100);
+  
+  }
+  digitalWrite(13, LOW);
+  return(0);
+}  
+ 
+
+
