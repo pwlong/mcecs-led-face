e57f915dc7c83b27dfa8ce255fd30b369a330188
pwlong <github@thelongs.ws>
2014-11-07 09:50:56 -0800

    first stab at strings coming in for control (instead of just char)

--- py_eyes/py_eyes.ino
+++ py_eyes/py_eyes.ino
@@ -5,6 +5,7 @@
 #include "Adafruit_GFX.h"
 #include "Animation_Frames.h"
 
+
 // Because the two eye matrices share the same address, only four
 // matrix objects are needed for the five displays:
 #define MATRIX_EYES         0
@@ -32,9 +33,7 @@ uint8_t
   mouthCountdown =  10; // Countdown to next mouth change
 int8_t
   eyeX = 3, eyeY = 3;   // Current eye position
-/*  newX = 3, newY = 3,   // Next eye position
-  dX   = 0, dY   = 0;   // Distance from prior to new position
-*/
+  
 uint8_t
   pupilX  = 3, pupilY = 3,
   pupilSize = 2;
@@ -79,9 +78,27 @@ void loop() {
 
   // make a pupil by filling a rectangle with dark pixels
   matrix[MATRIX_EYES].fillRoundRect(pupilX, pupilY, pupilSize, pupilSize, 1, LED_OFF);
-  for(uint8_t i=0; i<4; i++) matrix[i].writeDisplay();
+  for(uint8_t i=0; i<4; ++i) matrix[i].writeDisplay();
   delay(20); // ~50 FPS
 
+  static char buffer[80];
+    if (readline(Serial.read(), buffer, 80) >0) {
+      switch(buffer){
+        case "top_lid_down":
+          for (uint8_t ii=0; ii<4; ++ii){
+            matrix[MATRIX_EYES].drawBitmap (0, 0, top_lid_down[ii], 8, 8, LED_ON);
+            delay(20);
+          }
+          delay(500);
+          break;
+        
+        default: /* nothing to do yet */; break;
+      }
+    }
+
+
+
+/*
   if(Serial.available()){
     char inByte = Serial.read();
     switch (inByte){
@@ -94,60 +111,8 @@ void loop() {
       case 'r':      // fall through to reset
       default:  pupilX = pupilY = 3; pupilSize = 2; break;
     }
-  }
+  }*/
 }
 
 
-int post(Adafruit_8x8matrix matrix) {
-  // Right now this test always passes and it is up to the user to observe the output
-  // to determine if overything went OK
-  matrix.clear();
-  matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON);
-  matrix.writeDisplay();
-  delay(500);
-
-  matrix.clear();
-  matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON);
-  matrix.writeDisplay();
-  delay(500);
-
-  matrix.clear();
-  matrix.drawBitmap(0, 0, frown_bmp, 8, 8, LED_ON);
-  matrix.writeDisplay();
-  delay(500);
-
-  matrix.clear();      // clear display
-  matrix.drawPixel(0, 0, LED_ON);  
-  matrix.writeDisplay();  // write the changes we just made to the display
-  delay(500);
-
-  matrix.clear();
-  matrix.drawLine(0,0, 7,7, LED_ON);
-  matrix.writeDisplay();  // write the changes we just made to the display
-  delay(500);
-
-  matrix.clear();
-  matrix.drawRect(0,0, 8,8, LED_ON);
-  matrix.fillRect(2,2, 4,4, LED_ON);
-  matrix.writeDisplay();  // write the changes we just made to the display
-  delay(500);
-
-  matrix.clear();
-  matrix.drawCircle(3,3, 3, LED_ON);
-  matrix.writeDisplay();  // write the changes we just made to the display
-  delay(500);
-
-  matrix.setTextSize(1);
-  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
-  matrix.setTextColor(LED_ON);
-  for (int8_t x=0; x>=-36; x--) {
-    matrix.clear();
-    matrix.setCursor(x,0);
-    matrix.print("Hello");
-    matrix.writeDisplay();
-    delay(100);
-  }
-  return(0);
-}  
- 
   
